414bc2bf79bce8256e5ec59f26554b6b6890f0c0
Ildevertdaisy <ildevertdaisy@gmail.com>
2025-04-23 14:03:30 +0200

    Adding test image for OCR

--- main.py
+++ main.py
@@ -44,7 +44,18 @@ def main():
     )
     
 
-    prompt = """""" # To be defined later
+    prompt = """
+- Instrution: OCR the text from the image attached in markdown format. Make sure to only extract only the text and don't forget to consider and detect checkboxex in images."
+- Constrain:"
+   - Don't forget any text inside the image
+   - You only respond with the text extract in markdown.
+   - You also respect the table(s) structure(s) mentionned and reflect it in markdown
+   - Don't include the ```mardown <text extracted>``` delimiter in your response
+   - You also respect the checkmark in checkboxes presented.
+   - If there's a checked checbox, represents if with this character: ☑️
+   - If there's an unchecked checbox, represents if with this character: ☐
+""" 
+# To be defined later
 
 
     pipeline_options.vlm_options = ollama_vlm_options(
--- playground.py
+++ playground.py
@@ -2,12 +2,24 @@
 import ollama
 
 
+prompt = """
+- Instrution: Extract the text from the image attached in markdown format. Make sure to only extract only the text and don't forget to consider and detect checkboxex in images."
+- Constrain:"
+   - Don't forget any text inside the image
+   - You only respond with the text extract in markdown.
+   - You also respect the table(s) structure(s) mentionned and reflect it in markdown
+   - Don't include the ```mardown <text extracted>``` delimiter in your response
+   - You also respect the checkmark in checkboxes presented.
+   - If there's a checked checbox, represents if with this character: ☑️
+   - If there's an unchecked checbox, represents if with this character: ☐
+""" 
+
 response = ollama.chat(
     model='llama3.2-vision',
     messages=[
         {
             'role': 'user',
-            'content': 'What is in this image ?',
+            'content': prompt,
             'images': ['ball.jpg']
         }
     ]
